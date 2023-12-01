5e48189b065a14fdbab3b2f45f71cbc205431100
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-12-01 00:32:35 -0600

    small changes

---
+++ control-flow-and-collections/exercises/collections-exercises/collections-exercises/src/main/java/org/launchcode/Array.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+        for (int i : integerArray) {
+            System.out.println(i);
+        }
+    }
+}
